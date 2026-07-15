db0ef2dc08ff63853ece399f2f48e31d1560fdd6
Márcio <vi.nimarcio@hotmail.com>
2026-07-15 19:24:41 -0300

    feat: exporta model de transferência financeira

--- src/infrastructure/database/models/index.js
+++ src/infrastructure/database/models/index.js
@@ -1,9 +1,11 @@
 const Transaction = require('./TransactionModel');
 const Category = require('./CategoryModel');
 const BudgetRule = require('./BudgetRuleModel');
+const BudgetGroupTransfer = require('./BudgetGroupTransferModel');
 
 module.exports = {
     Transaction,
     Category,
     BudgetRule,
+    BudgetGroupTransfer,
 };
