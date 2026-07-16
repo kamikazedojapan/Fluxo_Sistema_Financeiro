a250b682b825c54ce3d3cd44907e7385e7183207
Márcio <vi.nimarcio@hotmail.com>
2026-07-16 16:55:11 -0300

    feat: exporta repository de transferencia financeira

--- src/infrastructure/database/repositories/index.js
+++ src/infrastructure/database/repositories/index.js
@@ -1,9 +1,11 @@
 const transactionRepository = require('./transactionRepository');
 const categoryRepository = require('./categoryRepository')
 const budgetRuleRepository = require('./budgetRuleRepository');
+const budgetGroupTransferRepository = require('./budgetGroupTransferRepository');
 
 module.exports = {
   transactionRepository,
   categoryRepository,
   budgetRuleRepository,
+  budgetGroupTransferRepository,
 };
